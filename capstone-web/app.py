978a3f26607feba1938c47a99b35504d0ad2005a
Rad710 <rolmedro@gmail.com>
2023-01-06 00:34:07 +0800

    webpage

---
+++ capstone-web/app.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:fa399591a410a1769f2fb4445965f4b8816e68b095f563edd266bdf92a61d67e
+size 4271
---
+++ capstone-web/app2.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:cc6a9f84c03f5b2d7ab25bd8d487dd9867dfcde235e2d3355e1dc208120ac0ee
+size 3833
